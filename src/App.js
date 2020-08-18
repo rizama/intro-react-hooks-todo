0cb07574e20afcbe2510908c54e29b0061f73c1f
rizky sam pratama <rizkysampratama@gmail.com>
2020-08-18 19:59:08 +0700

    remove some file, add some style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div>
+//       <h1>Hello React!</h1>
+//     </div>
+//   );
+// }
+
+// const App = () => (
+//   <div>
+//     <h1>Hello React!</h1>
+//   </div>
+// );
+const Header = <h1>Wow</h1>
+
+const App = () => {
+  const name = "sam";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello {name}!</h1>
     </div>
-  );
-}
+  )
+};
 
 export default App;
