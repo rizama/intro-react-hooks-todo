99beab6e3e6aa64391584c5f3312375d809bbfb9
rizky sam pratama <rizkysampratama@gmail.com>
2020-08-29 21:23:31 +0700

    mark all todo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React, { useState, useCallback, useEffect } from 'react';
 
-// function App() {
-//   return (
-//     <div>
-//       <h1>Hello React!</h1>
-//     </div>
-//   );
-// }
-
-// const App = () => (
-//   <div>
-//     <h1>Hello React!</h1>
-//   </div>
-// );
-
 const App = () => {
   // [default_value, function for set new value]
   const [newTodo, setNewTodo] = useState('');
   const [todos, setTodos] = useState([]);
-  
+
   const onNewTodoChange = useCallback((event) => {
     setNewTodo(event.target.value)
   }, []);
@@ -26,12 +12,13 @@ const App = () => {
   const formSubmitted = useCallback((click) => {
     click.preventDefault()
     if(!newTodo.trim()) return;
-    setTodos([...todos,
+    setTodos([
       {
-        id: todos.length + 1,
+        id: todos.length ? todos[0].id + 1 : 1,
         content: newTodo,
         done: false,
-      }
+      },
+      ...todos
     ])
     setNewTodo('');
   }, [newTodo, todos]);
@@ -43,11 +30,28 @@ const App = () => {
   const addTodo = useCallback((todo, index) => (event) => {
     const newTodos = [...todos];
     newTodos.splice(index, 1, {
-      ...todos,
+      ...todo,
       done: !todo.done
     })
     setTodos(newTodos)
   }, [todos]);
+
+  const removeTodo = useCallback(
+    (todo) => (event) => {
+      setTodos(todos.filter(otherTodo => otherTodo !== todo))
+    },
+    [todos]
+  );
+
+  const markAll = useCallback(() => {
+      const markTodos = todos.map((todo) => {
+        return {
+          ...todo,
+          done: true
+        }
+      })
+      setTodos(markTodos);
+    },[todos]);
 
   return (
     <div>
@@ -57,19 +61,23 @@ const App = () => {
           id="newTodo"
           name="newTodo"
           value={newTodo} 
+          className="input-form"
           onChange={onNewTodoChange} 
         />
-        <button>Add todo</button>
+        <button className="button-input">Add todo</button>
+        <button className="button-input" onClick={markAll}>Mark All Done</button>
       </form>
       <ul>
         {todos.map((todo, index) => (
           <li key={todo.id}>
             <input 
+              className=""
               checked={todo.done}
               type="checkbox"
               onChange={addTodo(todo, index)}
             />
-            <span className={todo.done ? 'done' : ''}>{todo.content}</span>
+            <span className={todo.done ? 'done' : ''}>  {todo.content}</span>
+            <button className="button-remove" onClick={removeTodo(todo)}>Remove Todo</button>
           </li>
         ))}
       </ul>
